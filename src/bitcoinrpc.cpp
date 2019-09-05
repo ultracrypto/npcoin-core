b5197e3c5bd39247faa7c5a6e045fc369976abb4
utracrypto <44044742+ultracrypto@users.noreply.github.com>
2019-09-05 15:01:02 +0800

    Update bitcoinrpc.cpp

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -43,6 +43,7 @@ void ThreadRPCServer3(void* parg);
 static inline unsigned short GetDefaultRPCPort()
 {
     return GetBoolArg("-testnet", false) ? 23131 : 13131;
+    
 }
 
 Object JSONRPCError(int code, const string& message)
